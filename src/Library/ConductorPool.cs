ebcc9e3cbd60cafb3828cc69f970fd7d05b5c536
Seba-Quintana <SEBASTIAN.QUINTANA@correo.ucu.edu.uy>
2022-05-12 19:42:55 -0300

    clases y net

---
+++ src/Library/ConductorPool.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+
+namespace PII_Herencia
+{
+    class ConductorPool : Conductor
+    {
+        
+    }
+}
---
+++ src/Library/Usuario.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+
+namespace PII_Herencia
+{
+    abstract class Usuario
+    {
+        
+    }
+}
